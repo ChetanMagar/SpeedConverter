22d6e48b56ff896f45de87199a99b51bf7f7881e
Chetan Magar <Magarchetan48@gmail.com>
2020-11-11 16:50:18 +0530

    Initial commit

---
+++ src/SpeedConverter.java
@@ -0,0 +1,19 @@
+public class SpeedConverter {
+    public static long toMilesPerHour(double kilometersPerHours){
+        if (kilometersPerHours <0) {
+            return -1;
+        }else {
+            return Math.round(kilometersPerHours/1.609);
+
+        }
+    }
+    public static void printConversion(double kilometersPerHours){
+        if (kilometersPerHours<0) {
+            System.out.println("Invalid Value");
+        }else {
+            long milesPerHours=toMilesPerHour(kilometersPerHours);
+            System.out.println(kilometersPerHours+" km/h = "+milesPerHours+" mi/h");
+        }
+    }
+
+}
